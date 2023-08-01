e02b6ec8bdac55cda766ca8c0c70bcbf7898ae6f
Shane Jonas <jonas.shane@gmail.com>
2023-08-01 15:56:55 -0400

    Removed unused getCurrentChainId

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -742,7 +742,6 @@ export default class MetamaskController extends EventEmitter {
         networkControllerMessenger,
         'NetworkController:networkDidChange',
       ),
-      getCurrentChainId: this.getCurrentChainId.bind(this),
       preferencesController: this.preferencesController,
       onboardingController: this.onboardingController,
       initState: initState.IncomingTransactionsController,
@@ -1180,7 +1179,6 @@ export default class MetamaskController extends EventEmitter {
           ({ networkId }) => networkId,
         );
       },
-      getCurrentChainId: this.getCurrentChainId.bind(this),
       preferencesStore: this.preferencesController.store,
       txHistoryLimit: 60,
       signTransaction: this.keyringController.signTransaction.bind(
